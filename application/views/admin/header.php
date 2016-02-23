2813b1af7d93f4218b0df8729d774f38433cd809
Minion <muhammad-ihsan@outlook.com>
2016-02-24 00:13:52 +0700

    disable Select2 on backend header

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -37,12 +37,12 @@
     <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/jquery.matchHeight-min.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/dataTables.bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/select2.full.min.js"></script>
+    <!-- <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/select2.full.min.js"></script> -->
     <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/ace/ace.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/ace/mode-html.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/ace/theme-github.js"></script>
     <!-- Javascript -->
-    <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/app.js"></script>
+    <!-- <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/app.js"></script> -->
     <script type="text/javascript" src="<?php echo base_url() ?>template/assets/js/index.js"></script>
 </head>
 

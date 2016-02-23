cbe49bc660e15189c494eb43e133a3e086e12037
Minion <muhammad-ihsan@outlook.com>
2016-02-23 15:02:35 +0700

    rhcloud service

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://isbanban.dev';
+$config['base_url'] = 'http://isbanban-barndev.rhcloud.com';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://isbanban.dev';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------

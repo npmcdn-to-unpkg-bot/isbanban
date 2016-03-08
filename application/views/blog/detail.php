e280d752a5b10f163b503ed5dd25181cbf9f6baa
Muhammad Ihsan <muhammad-ihsan@outlook.com>
2016-03-08 20:39:51 +0700

    improve blog detail frontend page

--- application/views/blog/detail.php
+++ application/views/blog/detail.php
@@ -1,17 +1,52 @@
 <div class="jumbotron blogpost" style="background:url(http://placemi.com/1280x600) no-repeat top center;">
-	<div class="shade"></div>
-	<div class="container">
-		<h2 class="title">Jumping Fox River</h2>
-	</div>
+	<div class="shade white"></div>
 </div>
 
 
 
-<div class="single-post">
+<div class="single-post blog">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-8">
+			<div class="col-sm-10 col-sm-offset-1">
+				<div class="meta">					
+					<h1 class="title">jumping fox river</h1>
+
+					<div class="postdate">
+						Posted: 25 Juni 2015
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-10 col-sm-offset-1">
 				<div class="post">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -20,15 +55,28 @@
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
 			</div>
+		</div>
+	</div>
+</div>
 
-			<div class="col-sm-4 sidebar ">
-				<div class="related-blog">
-					<div class="start">
-					<a href="#">
-						<img class="img-responsive" src="http://placemi.com/800x400"></img>
-						<h4 class="title">Another Jumping Fox</h4>
-					</a></div>
+
+<div class="another-post blog">
+	<div class="container">
+		<h3>Another Post</h3>
+
+		<div class="post">
+			<div class="row">
+				<?php for($i=0; $i<3; $i++) { ?>
+				<div class="col-sm-6 col-md-4">
+					<div class="begin-post blog" style="background:url(http://placemi.com/1280x500) no-repeat top center">
+						<a href="<?php echo base_url() ?>blog/detail/default">
+						<div class="shade"></div>
+						<div class="title">Jumping fox
+						</div>
+						</a>
+					</div>
 				</div>
+				<?php } ?>
 			</div>
 		</div>
 	</div>

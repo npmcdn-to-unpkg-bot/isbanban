9ef38ee4e4cde4c6f12982219b7e60fb9da47b25
Muhammad Ihsan <muhammad-ihsan@outlook.com>
2016-03-09 10:21:18 +0700

    add `clean` functions

--- application/libraries/Barnlibs.php
+++ application/libraries/Barnlibs.php
@@ -12,6 +12,58 @@ class Barnlibs {
 		echo $day."-".$month."-".$year;
     }
 
+    public function dateForHumanClean($date)
+    {
+    	$orderdate = explode('-', $date);
+		$year  = $orderdate[0];
+		$month   = $orderdate[1];
+		$day = $orderdate[2];
+
+		switch ($month) {
+			case 1:
+				$month = "Januari";
+				break;
+			case 2:
+				$month = "Februari";
+				break;
+			case 3:
+				$month = "Maret";
+				break;
+			case 4:
+				$month = "April";
+				break;
+			case 5:
+				$month = "Mei";
+				break;
+			case 6:
+				$month = "Juni";
+				break;
+			case 7:
+				$month = "Juli";
+				break;
+			case 8:
+				$month = "Agustus";
+				break;
+			case 9:
+				$month = "September";
+				break;
+			case 10:
+				$month = "Oktober";
+				break;
+			case 11:
+				$month = "November";
+				break;
+			case 12:
+				$month = "Desember";
+				break;
+
+			default:
+				break;
+		}
+
+		echo $day.", ".$month." ".$year;
+    }
+
     public function monthForHuman($date)
     {
     	$orderdate = explode('-', $date);
@@ -62,5 +114,57 @@ class Barnlibs {
 		}
 
 		echo $month."&mdash;".$year;
+    }
+
+    public function monthForHumanClean($date)
+    {
+    	$orderdate = explode('-', $date);
+		$year		= $orderdate[0];
+		$month 		= $orderdate[1];
+		$day		= $orderdate[2];
+
+		switch ($month) {
+			case 1:
+				$month = "Januari";
+				break;
+			case 2:
+				$month = "Februari";
+				break;
+			case 3:
+				$month = "Maret";
+				break;
+			case 4:
+				$month = "April";
+				break;
+			case 5:
+				$month = "Mei";
+				break;
+			case 6:
+				$month = "Juni";
+				break;
+			case 7:
+				$month = "Juli";
+				break;
+			case 8:
+				$month = "Agustus";
+				break;
+			case 9:
+				$month = "September";
+				break;
+			case 10:
+				$month = "Oktober";
+				break;
+			case 11:
+				$month = "November";
+				break;
+			case 12:
+				$month = "Desember";
+				break;
+
+			default:
+				break;
+		}
+
+		echo $month." ".$year;
     }
 }

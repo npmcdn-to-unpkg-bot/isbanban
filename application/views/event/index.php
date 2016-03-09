8171e0b15a789d6626a9ceca9f05127c5a4cc47f
Muhammad Ihsan <muhammad-ihsan@outlook.com>
2016-03-10 01:37:10 +0700

    improve events frontend page

---
+++ application/views/event/index.php
@@ -0,0 +1,53 @@
+<div class="jumbotron bigger" style="background: url(<?php echo base_url() ?>template/assets/image/bg-event.png) no-repeat top center;">
+	<div class="container">
+		<h1>Events</h1>
+		<p>Leading text event blog here</p>
+		<hr>
+	</div>
+</div>
+
+
+<div class="precontent">
+	<div class="container">
+		<h1>Mark your calendar</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam libero, quis nisi expedita perferendis voluptatem doloremque labore officiis aspernatur optio debitis placeat, atque, perspiciatis voluptas tempore. Cum repellendus, quas dolor.</p>
+	</div>
+</div>
+
+<div class="post">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="begin-post event">
+					<div class="row">
+						<div class="col-sm-5">
+							<div class="thumbnails">
+								<img src="http://placemi.com/800x400" alt="" class="img-responsive">
+							</div>
+						</div>
+
+						<div class="col-sm-7">
+							<h1>Jumping Fox River</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6">
+				<div class="begin-post event">
+					<div class="row">
+						<div class="col-sm-5">
+							<div class="thumbnails">
+								<img src="http://placemi.com/800x400" alt="" class="img-responsive">
+							</div>
+						</div>
+
+						<div class="col-sm-7">
+							<h1>Jumping Fox River</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>

bfc71cf074f629ca4b75961e6e93b9f8f7878477
Muhammad Ihsan <muhammad-ihsan@outlook.com>
2016-03-16 01:32:52 +0700

    update custom library

--- application/libraries/Barnlibs.php
+++ application/libraries/Barnlibs.php
@@ -175,48 +175,6 @@ class Barnlibs {
 		$month   = $orderdate[1];
 		$day 	 = $orderdate[2];
 
-		switch ($month) {
-			case 1:
-				$month = "Januari";
-				break;
-			case 2:
-				$month = "Februari";
-				break;
-			case 3:
-				$month = "Maret";
-				break;
-			case 4:
-				$month = "April";
-				break;
-			case 5:
-				$month = "Mei";
-				break;
-			case 6:
-				$month = "Juni";
-				break;
-			case 7:
-				$month = "Juli";
-				break;
-			case 8:
-				$month = "Agustus";
-				break;
-			case 9:
-				$month = "September";
-				break;
-			case 10:
-				$month = "Oktober";
-				break;
-			case 11:
-				$month = "November";
-				break;
-			case 12:
-				$month = "Desember";
-				break;
-
-			default:
-				break;
-		}
-
 		return $day;
     }
 

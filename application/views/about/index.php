f7b295ad6b385bf5f89142af5ec3397a3c43ed38
Muhammad Ihsan <muhammad-ihsa@outlook.com>
2016-04-09 03:20:16 +0700

    improve about page

--- application/views/about/index.php
+++ application/views/about/index.php
@@ -25,61 +25,48 @@
 						<h1>Profile</h1>      
 					</div>
 
-					<h4>Bagaimana Kondisi Pendidikan di banten</h4>
-					<p>
-						Banten dalam segi pendidikan masih dinobatkan sebagai salah satu provinsi yang memiliki
+					<h3>Bagaimana Kondisi Pendidikan di banten</h3>
+					<div class="post-text-content">
+						<p>
+							Banten dalam segi pendidikan masih dinobatkan sebagai salah satu provinsi yang memiliki
+							kualitas pendidikan yang rendah di Indonesia. Badan Pusat Statistik Banten tahun 2013
+							menyebutkan sebanyak 312.409 anak tidak bersekolah dari 604.812 total anak di Banten.
+							Artinya 51,6 % anak tidak menikmati akses pendidikan. Angka penyumbang terbesar berasal
+							dari pelosok desa di Banten.
+						</p>
 
-kualitas pendidikan yang rendah di Indonesia. Badan Pusat Statistik Banten tahun 2013
+						<p>
+							Rendahnya tingkat pendidikan di Banten juga menyebabkan sebanyak 337 ribu orang
+							mengalami buta huruf. Tidak hanya itu, rendahnya kualitas pendidikan ini menyebabkan
+							25.860 orang berada dibawah garis kemiskinan pada tahun 2015. Kemiskinan pada tahun
+							2015 meningkat 7,89 % dari tahun yang 2014 disebabkan oleh salah satunya karena faktor
+							rendahnya kualitas pendidikan di Banten khususnya di pelosok desa.
+						</p>
 
-menyebutkan sebanyak 312.409 anak tidak bersekolah dari 604.812 total anak di Banten.
-
-Artinya 51,6 % anak tidak menikmati akses pendidikan. Angka penyumbang terbesar berasal
-
-dari pelosok desa di Banten.
-
-Rendahnya tingkat pendidikan di Banten juga menyebabkan sebanyak 337 ribu orang
-
-mengalami buta huruf. Tidak hanya itu, rendahnya kualitas pendidikan ini menyebabkan
-
-25.860 orang berada dibawah garis kemiskinan pada tahun 2015. Kemiskinan pada tahun
-
-2015 meningkat 7,89 % dari tahun yang 2014 disebabkan oleh salah satunya karena faktor
-
-rendahnya kualitas pendidikan di Banten khususnya di pelosok desa.
-
-Rendahnya kualitas dan akses pendidikan di Banten memberikan dampak atas lemahnya
-
-Indeks Pertumbuhan Manusia (IPM) di Banten yang hanya mencapai angka 69.89.
-
-Penyumbang terbesar masih berasal dari pelosok desa yang minim akan akses pendidikan.
-
-Penyebab terjadinya hal tersebut adalah karena adanya ketidakseimbangan kualitas pendidikan di kota dan desa yang membuat adanya pandangan “ketidakadilan pendidikan” antara kota dan desa.
-					</p>
-
-					<h4>Bagaimana Cara menjadi <em>A PART OF CHANGE</em></h4>
-					<p>
-						Berbagai pandangan atas adanya “ketidakadilan akses dan kualitas pendidikan antara kota
-
-dan pelosok desa di Banten” membuat Istana Belajar Anak Banten (Isbanban) tergerak
-
-untuk menjadi “A Part of Change” melalui potensi anak mudanya dalam menciptakan
-
-akses dan kualitas pendidikan berkeadilan di pelosok desa Banten.
-
-Data tahun 2013 menyebutkan bahwa Banten memiliki 212 ribu anak muda yang tersebar di
-
-seluruh kabupaten/Kota. Hal tersebut membuat organisasi ini dipimpin dan digerakkan
-
-langsung oleh anak muda atau disebut youth-led organization dengan basis kerelawanan
-
-yang menerapkan social value “Care &amp; Share”. Kami selalu percaya bahwa anak muda
-
-memiliki peranan penting untuk mengubah suatu hal menjadi lebih baik. Dalam hal ini kami
-
-berani untuk menjadi ‘A Part of Change’ dengan mewadahi seluruh niat baik menjadi aksi
-
-baik demi pendidikan yang lebih baik di pelosok desa Banten.
-					</p>
+						<p>
+							Rendahnya kualitas dan akses pendidikan di Banten memberikan dampak atas lemahnya
+							Indeks Pertumbuhan Manusia (IPM) di Banten yang hanya mencapai angka 69.89.
+							Penyumbang terbesar masih berasal dari pelosok desa yang minim akan akses pendidikan.
+							Penyebab terjadinya hal tersebut adalah karena adanya ketidakseimbangan kualitas pendidikan di kota dan desa yang membuat adanya pandangan “ketidakadilan pendidikan” antara kota dan desa.
+						</p>
+					</div>
+							
+					<div class="post-text-content">
+						<h3>Bagaimana Cara menjadi <em>A PART OF CHANGE</em></h3>
+						<p>
+							Berbagai pandangan atas adanya “ketidakadilan akses dan kualitas pendidikan antara kota
+							dan pelosok desa di Banten” membuat Istana Belajar Anak Banten (Isbanban) tergerak
+							untuk menjadi “A Part of Change” melalui potensi anak mudanya dalam menciptakan
+							akses dan kualitas pendidikan berkeadilan di pelosok desa Banten.
+							Data tahun 2013 menyebutkan bahwa Banten memiliki 212 ribu anak muda yang tersebar di
+							seluruh kabupaten/Kota. Hal tersebut membuat organisasi ini dipimpin dan digerakkan
+							langsung oleh anak muda atau disebut youth-led organization dengan basis kerelawanan
+							yang menerapkan social value <b>Care &amp; Share</b>. Kami selalu percaya bahwa anak muda
+							memiliki peranan penting untuk mengubah suatu hal menjadi lebih baik. Dalam hal ini kami
+							berani untuk menjadi <b>A Part of Change</b> dengan mewadahi seluruh niat baik menjadi aksi
+							baik demi pendidikan yang lebih baik di pelosok desa Banten.
+						</p>
+					</div>
 				</section>
 
 				<section id="secVision">
@@ -94,25 +81,154 @@ baik demi pendidikan yang lebih baik di pelosok desa Banten.
 						<h1>Our Program</h1>
 					</div>
 
-					<div class="row">
-						<div class="col-sm-4">
-							<p class="lead">ACCESS <br>
-								"TAMAN BELAJAR"
-							</p>
-						</div>
+					<div class="post-text-content">
+						<h3>Access (Taman Belajar)</h3>
 
-						<div class="col-sm-4">
-							<p class="lead">LITERACY <br>
-								"MINGGU BELAJAR"
-							</p>
-						</div>
+						<p>
+							Akses pendidikan menjadi aspek penting untuk mendukung terealisasikannya pendidikan yang lebih baik di pelosok desa Banten. Isbanban bergerak dengan program pembentukan Taman Belajar Isbanban dengan tujuan untuk menciptakan wadah belajar bagi 455 anak usia 4 - 12 tahun di 8 desa binaan yang berlokasi di 7 Kabupaten/Kota Provinsi Banten.
+						</p>
 
-						<div class="col-sm-4">
-							<p class="lead">QUALITY <br>
-								"BEASISWA BELAJAR"
-							</p>
-						</div>
+						<p>
+							Di tahun 2013, kami berhasil mendirikan taman baca pertama kali di Kp.Sukamaju, Desa Citasuk, Kecamatan Padarincang – Kabupaten Serang. Dengan bermodalkan 40 panitia pelaksana yang berasal dari Forum OSIS Banten bekerjasama dengan Banten Muda Community, taman belajar pertama ini menjadi simbol lahirnya Istana Belajar Anak Banten tepat di tanggal 10 februari 2013. 
+						</p> 
+
+						<p>
+							Pada tahun 2014, kami bernisiatif untuk mengembangkan program taman belajar ke seluruh daerah Kabupaten/Kota di Provinsi Banten. Melalui acara Banten Youthcation 2014 pada tanggal 22 – 24 Juni 2014, kami mengundang anak muda potensial dari 7 kabupaten dan kota di Banten untuk berkumpul di suatu acara kerelawanan dalam rangka pelatihan pembentukan taman baca. Acara tersebut memberikan dampak dengan ditandainya pembentukan 7 taman baca di 7 Kabupaten/Kota Provinsi Banten di tanggal oktober 2014.
+						</p>
+
+						<p>
+							Berikut daftar taman belajar yang telah kami dirikan di Banten. 
+
+							<ol>
+								<li>Kp. Cempaka Salam, Desa Saninten, Kabupaten Pandeglang.</li>
+								<li>Kp.Sanding, Desa Sindangsari, Kecamatan Sajira, Kabupaten Lebak.</li>
+								<li>Kp.Salinggara, Desa Sayar, Kecamatan Tak – Takan, Kota Serang.</li>
+								<li>Kp.Tigamaya, Desa Telagaluhur, Kecamatan Waringinkurung, Kabupaten Serang.</li>
+								<li>Kp,Sukamaju, Desa Citasuk, Kecamatan Padarincang, Kabupaten Serang</li>
+								<li>Kp. Cipala, Desa Lebak Gede, Kecamatan Pulo Merak, Kota Cilegon.</li>
+								<li>Kp.Marga Sari RT.04/08 Kelurahan Curug Kulon, Kecamatan Curug, Kabupaten Tangerang</li>
+								<li>Jalan Aria Putra Gg. Swadaya 5, No. 45 Kedaung, Pamulang, Tangerang Selatan. 15415</li>
+							</ol>
+						</p>
+
+						<p>
+							More information about all villlage <a href="<?php echo base_url() ?>village">visit here</a>
+						</p>
 					</div>
+
+
+					<div class="post-text-content">
+						<h3>LITERACY (Minggu Belajar)</h3>
+
+						<p>
+							Kami berfokus pada peningkatan literasi anak di pelosok desa Banten dengan tujuan untuk mendorong kemampuan anak dalam membaca, menulis dan menghitung. Harapannya adalah mencegah terjadinya peningkatan buta aksara di Banten dari tahun ke tahun. 
+						</p>
+
+						<p>
+							Dengan adanya Taman Belajar yang berlokasi di 8 desa binaan, Isbanban menginisiasi membuat gerakan  “Minggu Belajar Isbanban”. Sebuah program yang digerakkan oleh seluruh relawan Kakak Pengajar setiap hari minggu untuk memberikan semangat, waktu dan keceriaan pada 455 adik-adik di 8 desa binaan. <br>
+							
+							Setiap hari minggu, 200 lebih relawan melakukan pengajaran di 7 chapter Isbanban dengan mata pelajaran yang berbeda-beda setiap chapternya. Berikut aspek pengajaran di 7 chapter:
+						</p>
+
+
+<!-- Program 2  -->
+						<table class="table table-bordered">
+							<thead>
+								<tr>
+									<th>Chapter</th>
+									<th>Matapelajaran (Minggu Belajar)</th>
+								</tr>
+							</thead>
+
+							<tbody>
+								<tr>
+									<td>Lebak</td>
+									<td>
+										Bhs.Inggris, Bhs.Indonesia, Matematika, Kesenian + Pengembangan Bakat Anak
+									</td>
+								</tr>
+
+								<tr>
+									<td>Pandeglang</td>
+									<td>
+										Tematik + Pengembangan Bakat Anak
+									</td>
+								</tr>
+
+								<tr>
+									<td>Kota Serang</td>
+									<td>
+										Kesenian, Bahasa Indonesia, Matematika, IPA + Pengembangan Bakat Anak
+									</td>
+								</tr>
+
+								<tr>
+									<td>Kabupaten Serang</td>
+									<td>
+										Kesenian, Bahasa Inggris, Pengetahuan Umum, Bahasa Indonesia + Pengembangan Bakat Anak
+									</td>
+								</tr>
+
+								<tr>
+									<td>Kabupaten Tangerang</td>
+									<td>
+										On Going Process (Pembentukan Taman Belajar Baru) + Pengembangan Bakat Anak
+									</td>
+								</tr>
+
+								<tr>
+									<td>Cilegon</td>
+									<td>
+										Pend.Agama Islam, IPS, IPA, Matematika + Pengembangan Bakat Anak
+									</td>
+								</tr>
+
+								<tr>
+									<td>Tangerang Selatan</td>
+									<td>
+										Pengetahuan Umum, Pend.Agama Islam, Matematika, Bhs.Inggris + Pengembangan Bakat Anak
+									</td>
+								</tr>
+							</tbody>							
+						</table>
+
+						<p>
+							Kamu bisa berkenalan dengan 200 lebih relawan Isbanban di 7 chapter <a href="<?php echo base_url() ?>people">disini</a>
+						</p>
+					</div>
+
+
+<!-- Program 3 -->
+					<div class="post-text-content">
+						<h3>QUALLITY (Beasiswa Belajar)</h3>
+
+						<p>
+							Peningkatan akses pendidikan melalui Taman Belajar dan didorong dengan adanya program Minggu Belajar membuat kami terus bergerak untuk menciptakan kualitas pendidikan yang lebih baik di pelosok desa Banten. 
+						</p>
+
+						<p>
+							Melihat lemahnya Indeks Pertumbuhan Manusia(IPM) di Banten yang hanya mencapai angka 69.89. Kami berusaha menciptakan kesempatan belajar berkelanjutan untuk adik didik berpotensi melalui <b>Beasiswa Belajar Isbanban</b>. 
+						</p>
+
+						<p>
+							Beasiswa Belajar ini diberikan kepada adik binaan Isbanban yang memiliki potensi untuk melanjutkan pendidikan sampai jenjang perguruan tinggi. Beasiswa ini memiliki tahapan diantaranya beasiswa tahap SD menuju SMP, SMP menuju SMA, dan SMA menuju Kuliah. 
+						</p>
+
+						<p>
+							Kami memberikan Beasiswa Belajar kepada calon penerima beasiswa berdasarkan indikator dibawah ini :
+
+							<ol>
+								<li>Tingkat kemampuan ekonomi keluarga</li>
+								<li>Motivasi calon penerima beasiswa</li>
+								<li>Perkembangan keaktifan calon penerima beasiswa selama proses program “Minggu Belajar”</li>
+							</ol>
+						</p>
+
+						<p>
+							Beasiswa ini di desain khusus untuk menjamin sustainability impact terhadap proses peningkatan kualitas pendidikan di pelosok desa Banten. Output dari beasiswa ini adalah mendorong anak pelosok desa berpotensi mendapatkan kesempatan melanjutkan pendidikan. Sehingga, outcome dapat terlihat ketika penerima beasiswa memperoleh wawasan dan keterampilan dari pendidikan lanjutan di perkuliahan atau pelatihan life skill yang berdampak pada peningkatan kesejahteraan ekonomi keluarga dan pembangunan kapasitas sumber daya manusia di pelosok desa Banten. 
+						</p>
+					</div>
+
 				</section>
 
 
@@ -121,17 +237,200 @@ baik demi pendidikan yang lebih baik di pelosok desa Banten.
 						<h1>Our Impact</h1>
 					</div>
 
-					<p>
-						Dampak adalah ukuran seberapa berhasil kami menciptakan sebuah gerakan. Ketiga
+					<div class="post-text-content">
+						<p>
+							Dampak adalah ukuran seberapa berhasil kami menciptakan sebuah gerakan. Ketiga program yang kami rancang ditujukan untuk mencapai visi besar organisasi yaitu “Menciptakan Akses & Kualitas Pendidikan yang berkeadilan di Pelosok desa Banten”.
+							Program Beasiswa Belajar Isbanban memberikan long term impact pada 3 (tiga) aspek diantaranya aspek “Pendidikan, Ekonomi dan Sosial”.
+						</p>
+					</div>
+				</section>
 
-program yang kami rancang ditujukan untuk mencapai visi besar organisasi yaitu
 
-“Menciptakan Akses &amp; Kualitas Pendidikan yang berkeadilan di Pelosok desa Banten”.
+<!-- Section Team -->
+				<section id="secOurTeam">
+					<div class="page-header">
+						<h1>Our Team</h1>
+					</div>
 
-Program Beasiswa Belajar Isbanban memberikan long term impact pada 3 (tiga) aspek
+					<div class="post-text-content">
+						<p>
+							Gerakan kerelawanan ini ditentukan dari seberapa kuat dan hebat tim yang ada didalamnya. Kami bergerak menjadi tim yang terdiri dari anak-anak muda yang memilki semangat dan inisiatif untuk menjadi <b>a part of change</b>. <br>
+							Perkenalkan tim kami :
+						</p>
 
-diantaranya aspek “Pendidikan, Ekonomi dan Sosial”.
-					</p>
+						<table class="table table-bordered">
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Roles</th>
+								</tr>
+							</thead>
+
+							<tbody>
+								<tr>
+									<td>Panji Aziz Pratama</td>
+									<td>Managing Director</td>
+								</tr>
+
+								<tr>
+									<td>Firmansyah</td>
+									<td>Vice Managing Director</td>
+								</tr>
+
+								<tr>
+									<td>Nia Dwi Anggraeni</td>
+									<td>1st General Secretary</td>
+								</tr>
+
+								<tr>
+									<td>Ima Gustiana</td>
+									<td>2nd General Secretary</td>
+								</tr>
+
+								<tr>
+									<td>Erlina</td>
+									<td>1st Finance</td>
+								</tr>
+
+								<tr>
+									<td>Ulfah</td>
+									<td>2nd Finance</td>
+								</tr>
+
+
+								<tr>
+									<th colspan="3">Child Development Departemen</th>
+								</tr>
+
+								<tr>
+									<td>Atika Suri (Project Manager Child Development)</td>
+									<td>Project Manager Child Development</td>
+								</tr>
+
+								<tr>
+									<td>Sakiban</td>
+									<td>Co. Learning Center</td>
+								</tr>
+
+								<tr>
+									<td>Aida Amalia</td>
+									<td>Co. Curriculum</td>
+								</tr>
+
+								<tr>
+									<td>Iqlima</td>
+									<td>Co. Talent Development</td>
+								</tr>
+
+
+								<tr>
+									<th colspan="3">Volunteer Resource Development Departemen</th>
+								</tr>
+
+								<tr>
+									<td>Ervi Cahyatri</td>
+									<td>Project Manager</td>
+								</tr>
+
+								<tr>
+									<td>Teja Saftiadi</td>
+									<td>Project Officer (ISBANBAN Chapter Lebak)</td>
+								</tr>
+
+								<tr>
+									<td>Teja Saftiadi</td>
+									<td>Project Officer (ISBANBAN Chapter Lebak)</td>
+								</tr>
+
+								<tr>
+									<td>Anis Fauziah</td>
+									<td>Project Officer (ISBANBAN Chapter Kota Serang)</td>
+								</tr>
+
+								<tr>
+									<td>Suhendi</td>
+									<td>Project Officer (ISBANBAN Chapter Pandeglang)</td>
+								</tr>
+
+								<tr>
+									<td>Catur Wahyono</td>
+									<td>Project Officer (ISBANBAN Chapter Kab.Tangerang)</td>
+								</tr>
+
+								<tr>
+									<td>Ulil Anshori</td>
+									<td>Project Officer (ISBANBAN Chapter Cilegon)</td>
+								</tr>
+
+								<tr>
+									<td>Endah Khaeriyah Nurhidayatin</td>
+									<td>Project Officer (ISBANBAN Chapter Kab.Serang)</td>
+								</tr>
+
+								<tr>
+									<td>Ainna Fisabila</td>
+									<td>Project Officer (ISBANBAN Chapter Tangerang Selatan)</td>
+								</tr>
+
+
+								<tr>
+									<th colspan="3">Public Relation Departemen</th>
+								</tr>
+
+								<tr>
+									<td>Nourul Fatimah</td>
+									<td>Project Manager</td>
+								</tr>
+
+								<tr>
+									<td>Hafidz Taqiudin</td>
+									<td>Coordinator of Partnership</td>
+								</tr>
+
+								<tr>
+									<th colspan="3">IT &amp; Media Departemen</th>
+								</tr>
+
+								<tr>
+									<td>Viriyanto</td>
+									<td>Project Manager</td>
+								</tr>
+
+								<tr>
+									<th colspan="3">Isbanban Community Departemen</th>
+								</tr>
+
+								<tr>
+									<td>M. Rylo Pambudi</td>
+									<td>Project Manager</td>
+								</tr>
+
+								<tr>
+									<td>Aldi Reinaldi</td>
+									<td>Coordinator of Community Journalistic</td>
+								</tr>
+
+								<tr>
+									<th colspan="3">Fundraising Departemen</th>
+								</tr>
+
+								<tr>
+									<td>Ega Merfiana</td>
+									<td>Project Manager</td>
+								</tr>
+
+								<tr>
+									<td></td>
+									<td>Coordinate of Care Cloth</td>
+								</tr>
+
+								<tr>
+									<td></td>
+									<td>Coordinate of Pingsan</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</section>
 			</div>
 		</div>
@@ -150,9 +449,15 @@ $body.scrollspy({
 	target: '#sectionNav',
 });
 
-$("#sectionNav").stick_in_parent({
-	parent: '.parent_sticky',
-	offset_top: 100
-});
+if ($(window).width() > 768) {
+	doStick();
+}
+
+function doStick() {
+	$("#sectionNav").stick_in_parent({
+		parent: '.parent_sticky',
+		offset_top: 100
+	});
+}
 
 </script>

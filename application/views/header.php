2c6e9596bbde46bd6b62af9199dba474060925f1
Muhammad Ihsan <muhammad-ihsa@outlook.com>
2016-04-10 17:36:34 +0700

    improve meta for goddamn see

--- application/views/header.php
+++ application/views/header.php
@@ -5,6 +5,37 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<title><?php echo $title; ?> &mdash; Istana Belajar Anak Banten</title>
 
+	<meta name="description" content="We Are Youth Led-Organization That Focus On Helping Education Of Children In Rural Areas.">
+
+    <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="Home - Istana Belajar Anak Banten">
+    <meta itemprop="description" content="We Are Youth Led-Organization That Focus On Helping Education Of Children In Rural Areas.">
+    <meta itemprop="image" content="http://walldeco.embrio.me/storage/app/media/meta-image.png">
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@">
+    <meta name="twitter:title" content="Home - Istana Belajar Anak Banten">
+    <meta name="twitter:description" content="We Are Youth Led-Organization That Focus On Helping Education Of Children In Rural Areas.">
+
+    <!-- Twitter summary card with large image must be at least 280x150px -->
+    <meta name="twitter:image:src" content="http://walldeco.embrio.me/storage/app/media/meta-image.png">
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Home - Istana Belajar Anak Banten">
+    <meta property="og:url" content="http://walldeco.embrio.me">
+    <meta property="og:image" content="http://walldeco.embrio.me/storage/app/media/meta-image.png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:description" content="We Are Youth Led-Organization That Focus On Helping Education Of Children In Rural Areas.">
+    <meta property="og:site_name" content="Istana Belajar Anak Banten">
+
+    <!-- Facebook Data -->
+    <meta property="fb:app_id" content="461752984031406">
+    <meta property="fb:page_id" content="1023391861033867">
+
+    <link rel="canonical" href="http://walldeco.embrio.me">
+
 	<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">

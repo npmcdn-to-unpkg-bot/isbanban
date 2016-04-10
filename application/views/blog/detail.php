af10876638ce88bb128db1b2daf0f197c947bd2c
Muhammad Ihsan <muhammad-ihsa@outlook.com>
2016-04-11 02:26:56 +0700

    improve shareholic inline

--- application/views/blog/detail.php
+++ application/views/blog/detail.php
@@ -30,7 +30,7 @@
 		</div>
 
 <!-- Shareholic -->
-		<div class='shareaholic-canvas' data-app='share_buttons' data-app-id=''></div>
+		<div class='shareaholic-canvas' data-app='share_buttons' data-app-id='24303855' expr:data-title='data:post.title' expr:data-link='data:post.url'></div>
 <!-- Disquss Comment -->
 		<hr>
 		<div id="disqus_thread"></div>

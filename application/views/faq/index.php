4b33f542fbe1ec912c9b0666f3c48203cfb9502f
Muhammad Ihsan <muhammad-ihsan@outlook.com>
2016-04-17 01:34:49 +0700

    add FAQ controller and view

---
+++ application/views/faq/index.php
@@ -0,0 +1,512 @@
+<!-- <div class="jumbotron bigger" style="background: url(<?php echo base_url() ?>template/assets/image/bg-blog.jpg) no-repeat top center;"> -->
+<div class="jumbotron bigger" style="background: url(http://unsplash.it/1280/800?random&blur) no-repeat center center;">
+	<div class="container">
+		<h1>Fraquently Asked Question</h1>
+		<p class="lead">Leading text about blog here</p>
+		<hr>
+	</div>
+</div>
+
+
+<section>
+	<div class="container">
+		<div class="row grid-masonry">
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Apa Itu Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Istana Belajar Anak Banten (Isbanban) adalah youth-led organization berbasis
+							gerakan kerelawanan yang memiliki visi untuk menciptakan akses dan kualitas
+							pendidikan yang berkeadilan di seluruh pelosok desa Banten. Isbanban telah
+							berbadan hukum Yayasan yang disahkan melalui Surat Keputusan Menteri Hukum
+							dan Hak Asasi Manusia No.AHU-0008514.AH.01.04 Tahun 2016.
+							Nama Istana Belajar merupakan perwujudan dari usaha anak muda untuk
+							menciptakan “Istana” bagi anak-anak pelosok desa dalam mengakses pendidikan
+							untuk “Belajar”.
+						</p>
+
+						<p>
+							Isbanban adalah organisasi yang menerapkan sistem kerelawanan dengan social
+							value “Care and Share” sebagai simbol kontribusi anak muda dengan cara berbagi
+							dan peduli terhadap pendidikan anak di pelosok desa Banten.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Visi
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Istana Belajar Anak Banten (Isbanban) tergerak untuk menjadi “A Part of Change”
+							melalui potensi anak mudanya dalam menciptakan akses dan kualitas pendidikan
+							berkeadilan di pelosok desa Banten
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Apa saja program Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							<ul>
+								<li>
+									<b>Ruang Belajar</b> <br>
+									Bantu Anak Desa punya ruang belajar berupa taman baca.
+									Gerakan ini diciptakan untuk membangun akses pendidikan berupa taman baca
+									berbasis literasi bagi anak desa untuk dapat mengakses berbagai buku bacaan dan
+									meningkatkan minat belajar serta partisipasi anak dalam membaca.
+								</li>
+
+								<li>
+									<b>Minggu Belajar</b> <br>
+									Bantu anak desa bisa belajar dengan mendatangkan relawan
+									muda Minggu Belajar merupakan gerakan pengajaran kreatif yang dilakukan oleh
+									relawan kakak pengajar kepada 455 adik binaan di 8 desa yang berlokasi di 7
+									kabupaten/kota provinsi Banten
+								</li>
+
+								<li>
+									<b>Beasiswa Belajar</b> <br>
+									Bantu anak desa melanjutkan pendidikannya dengan beasiswa
+									Beasiswa Belajar merupakan beasiswa yang diberikan kepada anak-anak berpotensi
+									yang berasal dari 8 desa binaan untuk mendapatkan akses pendidikan berupa
+									beasiswa melanjutkan sekolah
+								</li>
+							</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Apa itu First Meet Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							First Meet dalah program yang mempertemukan relawan Istana Belajar Anak Banten untuk
+							menjalin komunikasi dan silaturahmi. Bentuk acara First Meet Isbanban adalah :
+
+							<ul>
+								<li>Sosialisasi Istana Belajar Anak Banten</li>
+								<li>Meet and Greet relawan Isbanban</li>
+								<li>Games dan Focus Group Discussion</li>
+								<li>Group Interview</li>
+							</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Siapa itu relawan Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Relawan Isbanban adalah anak muda usia 15 - 25 tahun yang berdomisili di provinsi Banten
+							yang bersedia menjadi relawan Isbanban di beberapa bidang di bawah ini :
+						</p>
+
+						<p>
+							<ul>
+								<li>
+									<b>Relawan Pengajar</b> <br>
+									Relawan pengajar adalah anak muda yang bersedia untuk melakukan pengajaran setiap
+									hari minggu di salah satu dari 7 (tujuh) chapter yang berlokasi di 7 kabupaten/kota
+									provinsi Banten
+								</li>
+
+								<li>
+									<b>Relawan Fotografer</b> <br>
+									Relawan fotografer adalah anak muda yang bersedia untuk mendokumentasikan
+									pengajaran berbentuk foto setiap hari minggu di salah satu dari 7 (tujuh) chapter yang
+									berlokasi di 7 kabupaten/kota provinsi Banten.
+								</li>
+
+								<li>
+									<b>Relawan Videografer</b> <br>
+									Relawan videografer adalah anak muda yang bersedia untuk mendokumentasikan
+									pengajaran berbentuk video setiap hari minggu di salah satu dari 7 (tujuh) chapter yang
+									berlokasi di 7 kabupaten/kota provinsi Banten.
+								</li>
+
+								<li>
+									<b>Relawan Desaingrafer</b> <br>
+									Relawan desaingrafer adalah anak muda yang bersedia untuk membuat publikasi
+									berbentuk poster dalam setiap program yang dilaksanakan oleh Istana Belajar Anak
+									Banten di 7 (tujuh) chapter yang berlokasi di 7 kabupaten/kota provinsi Banten.
+								</li>
+							</ul>
+						</p>
+
+						<p>
+							<b>Catatan:</b> <br>
+							<em>
+								Relawan fotografer, videografer, desaingrafer termasuk dalam tim publikasi dan
+								dokumentasi di bawah departement IT &amp; Media Isbanban Foundation. Tim ini akan
+								melakukan berbagai project yang terkait dengan publikasi dan dokumentasi.
+							</em>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Siapa yang menjadi sasaran program Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Isbanban dipimpin dan digerakkan langsung oleh relawan muda usia 15 - 25 tahun.
+							Penerima manfaat Isbanban adalah anak-anak pelosok desa yang minim akan akses
+							pendidikan seperti buku bacaan, tenaga pengajar, kesempatan melanjutkan sekolah
+							dan minimnya pusat belajar bagi anak. Sasarannya merupakan anak usia 6 - 11 tahun
+							yang duduk di bangku sekolah dasar. Saat ini total penerima manfaat berjumlah 455
+							anak di 8 desa yang tersebar di 7 kabupaten/kota Provinsi Banten.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Mengapa Isbanban bergerak dalam ranah pendidikan di pelosok desa?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Banten dalam segi pendidikan masih dinobatkan sebagai salah satu provinsi yang
+							memiliki kualitas pendidikan yang rendah di Indonesia. Badan Pusat Statistik Banten
+							tahun 2013 menyebutkan sebanyak 312.409 anak tidak bersekolah dari 604.812
+							total anak di Banten. Artinya 51,6 % anak tidak menikmati akses pendidikan. Angka
+							penyumbang terbesar berasal dari pelosok desa di Banten.
+						</p>
+
+						<p>
+							Rendahnya tingkat pendidikan di Banten juga menyebabkan sebanyak 337 ribu orang
+							mengalami buta huruf. Tidak hanya itu, rendahnya kualitas pendidikan ini
+							menyebabkan 25.860 orang berada dibawah garis kemiskinan pada tahun 2015.
+							Kemiskinan pada tahun 2015 meningkat 7,89 % dari tahun yang 2014 disebabkan
+							oleh salah satunya karena faktor rendahnya kualitas pendidikan di Banten khususnya
+							di pelosok desa.
+						</p>
+
+						<p>
+							Rendahnya kualitas dan akses pendidikan di Banten memberikan dampak atas
+							lemahnya Indeks Pertumbuhan Manusia (IPM) di Banten yang hanya mencapai
+							angka 69.89. Penyumbang terbesar masih berasal dari pelosok desa yang minim
+							akan akses pendidikan. Penyebab terjadinya hal tersebut adalah karena adanya
+							ketidakseimbangan kualitas pendidikan di kota dan desa yang membuat adanya
+							pandangan “ketidakadilan pendidikan” antara kota dan desa.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						BAGAIMANA KAMI MENJADI <b>A PART OF CHANGE</b> ?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Berbagai pandangan atas adanya <b>ketidakadilan akses dan kualitas pendidikan
+							antara kota dan pelosok desa di Banten</b> membuat Istana Belajar Anak Banten
+							(Isbanban) tergerak untuk menjadi <b>A Part of Change</b> melalui potensi anak
+							mudanya dalam menciptakan akses dan kualitas pendidikan berkeadilan di pelosok
+							desa Banten.
+						</p>
+
+						<p>
+							Data tahun 2013 menyebutkan bahwa Banten memiliki 212 ribu anak muda yang
+							tersebar di seluruh kabupaten/Kota. Hal tersebut membuat organisasi ini dipimpin
+							dan digerakkan langsung oleh anak muda atau disebut youth-led organization
+							dengan basis kerelawanan yang menerapkan social value <b>Care &amp; Share</b>. Kami
+							selalu percaya bahwa anak muda memiliki peranan penting untuk mengubah suatu
+							hal menjadi lebih baik. Dalam hal ini kami berani untuk menjadi <b>A Part of Change</b>
+							dengan mewadahi seluruh niat baik menjadi aksi baik demi pendidikan yang lebih
+							baik di pelosok desa Banten.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Kapan Isbanban didirikan?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Isbanban berdiri pada tanggal 10 Februari 2013 dengan berdirinya children learning
+							center pertama di Kp. Sukamaju, Desa Citasuk, Kecamatan Padarincang – Kabupaten
+							Serang, Banten.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Kapan relawan melakukan pengajaran?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Relawan pengajar melakukan pengajaran setiap hari minggu di 7 chapter yang berlokasi di7
+							kabupaten/kota provinsi Banten
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Berapa lama masa pengabdian menjadi relawan di Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Masa pengabdian relawan sekurang-kurangnya adalah satu tahun terhitung sejak
+							dilaksanakannya first meet Istana Belajar Anak Banten
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Dimana lokasi Kantor Sekretariat Isbanban Foundation?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Kantor Sekretariat Isbanban berlokasi di Jl. Karya Bhakti 2A No. 98, Komplek KPPN,
+							Ciceri, Serang, Banten, 42151
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Dimana lokasi Kantor Sekretariat Isbanban Foundation?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Kantor Sekretariat Isbanban berlokasi di Jl. Karya Bhakti 2A No. 98, Komplek KPPN,
+							Ciceri, Serang, Banten, 42151
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Dimana lokasi desa binaan Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Istana Belajar Anak Banten berlokasi di 8 desa binaan yang berlokasi di 7 kabupaten/kota
+							provinsi Banten.
+						</p>
+
+						<p>
+							<ul>
+								<li>Kp. Cempaka Salam, Desa Saninten, Kabupaten Pandeglang.</li>
+								<li>Kp.Sanding, Desa Sindangsari, Kecamatan Sajira, Kabupaten Lebak.</li>
+								<li>Lingkungan Dalung RT.01/01 Kelurahan Dalung, Kota Serang</li>
+								<li>Kp.Tigamaya, Desa Telagaluhur, Kecamatan Waringinkurung, Kabupaten Serang.</li>
+								<li>Kp.Sukamaju, Desa Citasuk, Kecamatan Padarincang, Kabupaten Serang</li>
+								<li>Kp. Cipala, Desa Lebak Gede, Kecamatan Pulo Merak, Kota Cilegon.</li>
+								<li>Kp.Marga Sari RT.04/08 Kelurahan Curug Kulon, Kecamatan Curug, Kabupaten Tangerang</li>
+								<li>Jalan Aria Putra Gg. Swadaya 5, No. 45 Kedaung, Pamulang, Tangerang Selatan. 15415</li>
+							</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Dimana lokasi First Meet 24 April 2016 dilaksanakan?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Lokasi first meet akan diselenggarakan di Kota Serang (Alamat lengkap first meet akan kami
+							informasikan melalui SMS ketika kamu sudah melakukan pendaftaran online)
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Dimana lokasi First Meet 24 April 2016 dilaksanakan?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Lokasi first meet akan diselenggarakan di Kota Serang (Alamat lengkap first meet akan kami
+							informasikan melalui SMS ketika kamu sudah melakukan pendaftaran online)
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Bagaimana sistem pengajaran di Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Setiap chapter Istana Belajar Anak Banten memiliki sistem pengajaran yang berbeda-beda.
+							Penjelasan lebih lanjut tentang sistem pengajaran Isbanban akan dijelaskan pada saat first
+							meet.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Apa saja mata pelajaran yang diajarkan di Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Setiap chapter Istana Belajar Anak Banten memiliki mata pelajaran yang diajarkan,
+							diantaranya adalah:
+						</p>
+
+						<p>
+							<div class="table-responsive">
+								<table class="table table-bordered">
+									<thead>
+										<tr>
+											<th>Chapter</th>
+											<th>Mata Pelajaran Minggu Belajar</th>
+										</tr>
+									</thead>
+
+									<tbody>
+										<tr>
+											<td>Lebak</td>
+											<td>Bhs.Inggris, Bhs.Indonesia, Matematika, Kesenian + Pengembangan Bakat Anak</td>
+										</tr>
+
+										<tr>
+											<td>Pandeglang</td>
+											<td>Tematik + Pengembangan Bakat Anak</td>
+										</tr>
+
+										<tr>
+											<td>Kota Serang</td>
+											<td>Kesenian, Bahasa Indonesia, Matematika, IPA + Pengembangan Bakat Anak</td>
+										</tr>
+
+										<tr>
+											<td>Kabupaten Serang</td>
+											<td>Kesenian, Bahasa Inggris, Pengetahuan Umum, Bahasa Indonesia + Pengembangan Bakat Anak</td>
+										</tr>
+
+										<tr>
+											<td>Kabupaten Tangerang</td>
+											<td>On Going Process (Pembentukan Taman Belajar Baru) + Pengembangan Bakat Anak</td>
+										</tr>
+
+										<tr>
+											<td>Cilegon</td>
+											<td>Pend.Agama Islam, IPS, IPA, Matematika + Pengembangan Bakat Anak</td>
+										</tr>
+
+										<tr>
+											<td>Tangerang Selatan</td>
+											<td>Pengetahuan Umum, Pend.Agama Islam, Matematika, Bhs.Inggris + Pengembangan Bakat Anak</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Bagaimana ketentuan pengajaran di Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Ketentuan pengajaran akan dijelaskan lebih lanjut pada saat first meet. Karena setiap
+							chapter memiliki ketentuan masing-masing dalam melakukan pengajaran dan berbagai
+							program.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-6 masonry-item">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Bagaimana cara bergabung menjadi relawan Isbanban?
+					</div>
+
+					<div class="panel-body">
+						<p>
+							Kamu dapat mengakses di <a href="<?php echo base_url() ?>recruitment" target="_blank">recruitment.isbanban.org</a> untuk melakukan pendaftaran online sebagai relawan
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<script src="<?php echo base_url() ?>template/assets/vendor/masonry/dist/masonry.pkgd.min.js"></script>
+<script>
+$(document).ready(function(){
+	$('.grid-masonry').masonry({
+		itemSelector: '.masonry-item',
+	});
+});
+</script>

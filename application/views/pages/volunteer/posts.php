59bffa6b33dc9cc088c83eae257c7a3f20470471
Muhammad Ihsan <muhammad-ihsan@outlook.com>
2016-08-08 06:46:25 +0700

    update people layout frontend page

--- application/views/pages/volunteer/posts.php
+++ application/views/pages/volunteer/posts.php
@@ -1,5 +1,5 @@
 <a class="infinite-item" href="javascript:void(0);" onclick="throwModal('<?php echo $row->slug; ?>');">
-<div class="col-sm-3">
+<div class="col-sm-2 col-xs-6" style="padding:0px; margin-bottom: -20px">
 	<div class="people">
 		<?php if($row->path_foto) { ?>
 			<img class="img-responsive" src="<?php echo base_url() ?><?php echo $row->path_foto; ?>">

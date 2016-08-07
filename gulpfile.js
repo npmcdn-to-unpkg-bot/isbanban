55381448e640ac089fbca1ef5578486e18ec1207
Muhammad Ihsan <muhammad-ihsan@outlook.com>
2016-08-07 21:31:03 +0700

    update gulpfile (add some scripts for compile backendless)

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,23 @@ gulp.task('css', function() {
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
+gulp.task('css-backend', function() {
+  return gulp.src('template/assets/bracket/less/style.default.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]}))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('template/assets/bracket/css/'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('template/assets/bracket/css/'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
 
 gulp.task('watch-css', function() {
     gulp.watch('template/assets/css/site/*.less', ['css']);
+});
+
+gulp.task('watch-backend', function() {
+    gulp.watch('template/assets/bracket/less/*.less', ['css-backend']);
 });
